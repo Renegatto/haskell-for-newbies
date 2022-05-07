c098ef8e3205e9f7e17ba86e7e727b4b660fb77f
renegatto <edittorr@mail.ru>
2022-05-07 18:02:16 +0300

    Make TypeErrors for popular functions misuse

--- src/APrelude.hs
+++ src/APrelude.hs
@@ -1,5 +1,34 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 module APrelude (if_then_else) where
-
+import GHC.TypeLits (TypeError(..), ErrorMessage (Text, (:<>:), ShowType))
 if_then_else :: Bool -> a -> a -> a
 if_then_else True ifTrue _ = ifTrue
 if_then_else _ _ ifFalse = ifFalse
+
+instance 
+  TypeError (
+    'Text "Expected number, got function '"
+    :<>: 'ShowType (a -> b)
+    :<>: 'Text "'")
+  => Num (a -> b)
+instance 
+  TypeError (
+    'Text "Cannot compare functions of type '"
+    :<>: 'ShowType (a -> b)
+    :<>: 'Text "'")
+  => Eq (a -> b)
+instance 
+  TypeError (
+    'Text "Cannot compare functions of type '"
+    :<>: 'ShowType (a -> b)
+    :<>: 'Text "'")
+  => Ord (a -> b)
+instance 
+  TypeError (
+    'Text "Cannot show functions of type '"
+    :<>: 'ShowType (a -> b)
+    :<>: 'Text "'")
+  => Show (a -> b)
