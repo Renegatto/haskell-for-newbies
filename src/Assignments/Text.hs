5bdac7698a990c940531bb6cfc5d52dd2435a91d
renegatto <edittorr@mail.ru>
2022-11-28 19:20:50 +0300

    Translate Text assignment to Eng

--- src/Assignments/Text.hs
+++ src/Assignments/Text.hs
@@ -21,13 +21,8 @@ import Prelude
 import Data.Char (isUpper)
 import Text (firstSatisfies)
 
--- :seti -interactive-print=Text.Show.Unicode.uprint
-
--- a -> b -> c
--- a -> (b -> c)
-
--- a b c
--- ((a b) c)
+-- a -> b -> c = a -> (b -> c)
+-- a b c = ((a b) c)
 
 concat :: String -> String -> String
 concat = (++)
@@ -38,18 +33,19 @@ exclaim = \s -> concat s "!"
 
 -- | Поприветствовать
 greet :: String -> String
-greet = \name -> concat "Привет, мой друг " name
+greet = \name -> concat "Hi, my dear friend " name
 
+bob :: String
+bob = "Bob"
 
-поля = "Полина"
-
-_ = "Привет, мой друг Полина"
-
-_ = "Привет, мой друг Полина. Игнат женился на Сергее?"
+-- To do:
+_ = "Hi, my dear friend Bob"
+_ = "Hi, my dear friend Bob. Ignat have married Sergey?"
+  -- actually, "Have Ignat married Sergey" would be more correct but...
 
 -- | Поженились
 marry :: String -> (String -> String)
-marry = \one second -> concat one (concat " женился на " second)
+marry = \one second -> concat one (concat " have married " second)
 
 -- | Спросить
 ask :: String -> String
@@ -61,30 +57,33 @@ finish = flip concat "."
 space :: String -> String -> String
 space = \a -> concat (concat a " ")
 
-странныйВопрос = "А ты знала, что Евгений занимается евгеникой?"
-
--- * Assignment 2
+-- * Assignment 2a
 {-
-поШаблону "Полина" = "Привет, мой друг Полина. Игнат женился на Сергее?"
-поШаблону "Витя" = "Привет, мой друг Витя. Игнат женился на Сергее?"
+byTemplate "Bob" = "Hi, my dear friend Bob. Ignat have married Sergey?"
+byTemplate "Gregory" = "Hi, my dear friend Gregory. Ignat have married Sergey?"
 -}
 
-задание2готово :: Bool
-задание2готово = False
+byTemplate :: String -> String
+byTemplate = undefined
 
-поШаблону :: String -> String
-поШаблону = undefined
+{-
+byTemplate2 "Bob" "Charlie" "George" = "Hi, my dear friend Bob. Charlie have married George?"
+byTemplate2 "Gregory" "Eelon Musk" "you" = "Hi, my dear friend Gregory. Eelon Musk have married you?"
+-}
+
+byTemplate2 :: String -> String -> String
+byTemplate2 = undefined
 
 -- * Assignment 3
 {-
 Написякай функцию, которая берет на вход список имен и возвращает список фраз
-"по шаблону" для тех имен, которые начинаются с большой буквы.
+"by template" для тех имен, которые начинаются с большой буквы.
 
-фразыПоШаблону ["Витя","таня","Петя"] =
-  [ "Привет, мой друг Витя. Игнат женился на Сергее?"
-  , "Привет, мой друг Петя. Игнат женился на Сергее?" ]
+phrasesByTemplate ["Vitya","tanya","Cook"] =
+  [ "Hi, my dear friend Vitya. Ignat have married Sergey?"
+  , "Hi, my dear friend Cook. Ignat have married Sergey?" ]
 -}
--- char 'D'
+
 -- | является ли символ (Char) прописной буквой
 _ = isUpper :: Char -> Bool 
 _ = filter :: forall a. (a -> Bool) -> [a] -> [a]
@@ -94,14 +93,14 @@ _ = firstSatisfies :: forall a. (a -> Bool) -> [a] -> Bool
 -- | оператор композиции функций. `(f . g) x = f (g x)`
 _ = (.) :: forall a b c. (b -> c) -> (a -> b) -> a -> c
 
-фразыПоШаблону :: [String] -> [String]
-фразыПоШаблону = undefined
+phrasesByTemplate :: [String] -> [String]
+phrasesByTemplate = undefined
 
 -- * Assignment 4
 {-
-Напиши функцию f, которая:
+Напиши функцию weirdTemplate, которая:
 - принимает список имён
-- добавляет приставку "Сэр" ("Иван" |-> "Сэр Иван") ко всем именам
+- добавляет приставку "Sir" ("Ivan" |-> "Sir Ivan") ко всем именам
 - возвращает список всех получившихся фраз для этих имен, которые короче 49 символов
 -}
 _ = length :: forall a. [a] -> Int
@@ -117,5 +116,5 @@ dontTouch4 = (>=)
 dontTouch5 :: forall a. Ord a => a -> a -> Bool
 dontTouch5 = (==)
 
-f :: [String] -> [String]
-f = undefined
+weirdTemplate :: [String] -> [String]
+weirdTemplate = undefined
