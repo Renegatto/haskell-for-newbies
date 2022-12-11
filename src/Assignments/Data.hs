949320a02969774511a4fbb2c7babf4ecd4fc487
Alexey <alexey@mlabs.city>
2022-12-11 20:46:56 +0300

    Data Assignments WIP

--- src/Assignments/Data.hs
+++ src/Assignments/Data.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE StandaloneDeriving #-}
 module Assignments.Data where
 
 --------------------------------------
@@ -5,6 +6,9 @@ module Assignments.Data where
 
 data MyDataName =
   MyDataConstructor1 | MyDataConstructor2 | MyDataConstructor3 -- it has three constructors
+
+-- this brings into our scope all three data constructors. For example:
+_ = MyDataConstructor1 :: MyDataName
 
 example1 :: MyDataName
 example1 = MyDataConstructor1 -- this is an instance of MyDataName
@@ -17,9 +21,9 @@ example2 = MyDataConstructor2 -- this too
 example3 :: [MyDataName] 
 example3 = reverse [MyDataConstructor1,MyDataConstructor2]
 
+-- >>> deriving instance Show MyDataName -- we need this to print out MyDataName terms
 -- >>> example3
--- No instance for (Show MyDataName) arising from a use of `evalPrint'
--- In a stmt of an interactive GHCi command: evalPrint it_a17TU
+-- [MyDataConstructor2,MyDataConstructor1]
 
 {- ---------------------------
 
@@ -56,8 +60,67 @@ jackTheDog = undefined
 sandraPets :: [_]
 sandraPets = undefined
 
+
+
+
+
+
+---------------------------
+-- You also can pattern-match your datatypes in functions.
+
+
+data DirectionToGo = GoLeft | GoRight
+
+-- Say, we expect function "whereShouldIGo" to behave this way:
+
+-- >>> deriving instance Show DirectionToGo
+-- >>> whereShouldIGo GoRight
+-- "You go right."
+-- >>> whereShouldIGo GoLeft
+-- "You go left."
+
+-- Gladly, we can specify this directly:
+
+whereShouldIGo :: DirectionToGo -> String
+whereShouldIGo GoLeft = "You go left."
+whereShouldIGo GoRight = "You go right."
+
+{- Patterns matches from top to down, this is important.
+  if pattern doesn't match term, next pattern will compared against it, and so on.
+-}
+
+data Number = One | Two | Three | Four
+
+isItOneOrThree :: Number -> String
+isItOneOrThree Three = "Yes, it is three"
+isItOneOrThree One = "Yes, it is one"
+-- this is the "wildcard pattern", it matches everything. Here, it matches Two and Four.
+isItOneOrThree _ = "Nope, it's something else"
+
+-- >>> deriving instance Show DirectionToGo
+-- >>> isItOneOrThree Four
+-- "Nope, it's something else"
+-- >>> isItOneOrThree Three
+-- "Yes, it is three"
+
 {- ---------------------------
 
   Assignment 3
 
+  Given this table, define function "eatsKgOfFoodPerDay"
+ 
+  animal   | eats food per day, Kg
+  ------------------------
+  dogs     | 7
+  cats     | 1
+  turtles  | 3
+  rats     | 0.3
+  mouses   | 0.2
+  fishes   | 0.003
+  canaries | 0.1
+
 -}
+
+-- | How much given animal eats Kg of food per day
+eatsKgOfFoodPerDay :: _ -> Float
+eatsKgOfFoodPerDay = undefined
