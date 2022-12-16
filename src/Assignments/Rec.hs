03e38efe512e984f95f7461a96bf866a99312b64
polly <mondpolina@gmail.com>
2022-12-16 17:59:12 +0300

    DONE 1..2b assignments

--- src/Assignments/Rec.hs
+++ src/Assignments/Rec.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE DeriveFoldable #-}
 module Assignments.Rec where
 import Data.List.NonEmpty (NonEmpty)
 import qualified Data.List.NonEmpty as NonEmpty
 import APrelude (if_then_else)
+import Prelude hiding (concat)
 
-_ = foldl :: forall a b. (b -> a -> b) -> b -> [a] -> b
+
 
 {-@ -- Assigment 1a
 
@@ -13,16 +15,42 @@ sumStuff [] = 0
 @-}
 
 sumStuff :: [Integer] -> Integer
-sumStuff = undefined
+sumStuff = foldl (+) 0
+
+mulStuff :: [Integer] -> Integer
+mulStuff = foldl (*) 1
+
+-- >>> mulStuff [1,3,4,2,3]
+-- 72
+
 
 {-@ -- Assigment 1b
 
-glue ["ab","cd","325235"] == "abcd325235"
-
+glue ["ab","cd","325235"] == concat (concat (concat "" "ab")  "cd") "325235" -- "abcd325235"
+glue [] = ""
 @-}
 
+concat :: String -> String -> String
+concat a b = a ++ b
+
 glue :: [String] -> String
-glue = undefined
+glue = foldl concat ""
+
+
+-- >>> glue ["bleu","sky" , "very" ]
+
+{-@
+glueAny [['a','b'],"cd","325235"] == "abcd325235" -- [[Char]] -> [Char]
+glueAny [[True],[True,False],[],[False]] == [True,True,False,False] -- [[Bool]] -> [Bool]
+@-}
+
+q = [ 'a', 'b', 'c']
+
+-- >>> glueAny [['a','b'],"cd","325235"]
+-- "abcd325235"
+
+glueAny :: forall a. [[a]] -> [a]
+glueAny = foldl (++) []
 
 -- Part two
 
@@ -37,7 +65,7 @@ data NonEmpty a =
 
 @-}  
 
-
+_ = foldl :: forall a b. (b -> a -> b) -> b -> [a] -> b
 _ = foldl :: forall a b. (b -> a -> b) -> b -> NonEmpty a -> b
 
 {-@ -- Assigment 2
@@ -48,8 +76,50 @@ maximum (7 :| [19,23,9,-6]) == 23
 
 _ = NonEmpty.head :: forall a. NonEmpty a -> a
 
-maximum :: Ord a => NonEmpty a -> a
-maximum = undefined 
+isGreaterThan :: Ord a => a -> a -> Bool
+isGreaterThan a b = a > b
+
+maximum' :: Ord a => NonEmpty a -> a
+maximum' = \ass -> foldl max' (NonEmpty.head ass) ass 
+
+-- >>> maximum' (7 NonEmpty.:| [109,23,912,-6])
+-- 912
+
+{-@ -- Assigment 2
+
+maximumWithDefault 3 (7 :| [19,23,9,-6]) == 23
+maximumWithDefault 800 (7 :| [19,23,9,-6]) == 800
+maximumWithDefault 7 [19,23,9,-6] == 23
+maximumWithDefault 7 (Just 10) == 10
+maximumWithDefault 2 Nothing == 2
+@-}
+
+maximumWithDefault :: forall a f. (Ord a, Foldable f) => a -> f a -> a
+maximumWithDefault = foldl max'
+
+{-@
+maximum = if_then_else _ _ _
+-- значит, что мы что-то возвращаем функцию в зависимости от условия,
+-- причем всегда одно и то же
+
+foldl []
+-- не имеет смысла, т.к. [] - не функция
+
+NonEmpty.head _ _
+-- не может быть, т.к.
+  NonEmpty.head :: NonEmpty a -> a
+-- а не
+  NonEmpty.head :: _ -> _ -> _
+
+NonEmpty.head (>)
+-- не имеет смысла, т.к. 
+  (>) :: Ord a => a -> a -> Bool
+  -- а не
+  (>) :: NonEmpty a
+@-}
+
+-- эта функция - не пример, а инструмент
+max' = \a b -> if_then_else (a > b) a b
 
 -- ===============================
 -- RECURSION
